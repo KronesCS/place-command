9042df316b9ff78b83a854468b59dcd0c186fac5
KronesCS <115038079+KronesCS@users.noreply.github.com>
2022-12-07 09:43:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,52 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f . . . f f . . . . . 
+    . . . . f 1 . . . 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 5 3 5 3 d f f . . 
+    . . f d d f 3 5 5 5 f d d f . . 
+    . . . f f 3 3 5 2 5 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let turtle = sprites.create(img`
+    . . . . c c c c c c . . . . . . 
+    . . . c 6 7 7 7 7 6 c . . . . . 
+    . . c 7 7 7 7 7 7 7 7 c . . . . 
+    . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+    . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+    . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+    . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+    . . f 7 7 7 7 6 c 7 7 6 f c . . 
+    . . . f c c c c 7 7 6 f 7 7 c . 
+    . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+    . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+    c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+    f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+    . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+    . . c c c c c c c c c f . . . . 
+    `, SpriteKind.Enemy)
+turtle.setBounceOnWall(true)
+mySprite.setPosition(126, 38)
+tiles.setCurrentTilemap(tilemap`level1`)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 5))
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
+game.splash("Get ready", "")
+let music2 = 1
+forever(function () {
+    if (music2 == 1) {
+        music.playMelody("C5 B A G F E D C ", 120)
+    }
+})
